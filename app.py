bdc7bcca1d3f7ff137b6417f603b27c6700726dd
debanjanroy671-coder <debanjanroy671@gmail.com>
2026-09-02 22:30:48 +0530

    Fix CSS loading

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__, template_folder=".")
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/style.css")
+def style():
+    return send_from_directory(".", "style.css")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
